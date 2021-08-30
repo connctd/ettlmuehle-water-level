25a96b4c72f01033e83355f5bfdecf96dec505b5
Tobiasz Walczak <tobiaszwalczak@gmail.com>
2021-08-31 00:42:40 +0200

    Improve wording in DataTable

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -14,8 +14,8 @@ const DataTable: React.FC<DataTableProps> = ({ dateHeader, data, style }) => {
   const rows = data.map(({ date, level1, level2 }) => (
     <Table.Row key={date}>
       <Table.Cell>{date}</Table.Cell>
-      <Table.Cell>{level1}</Table.Cell>
-      <Table.Cell>{level2}</Table.Cell>
+      <Table.Cell>{level1} cm</Table.Cell>
+      <Table.Cell>{level2} cm</Table.Cell>
     </Table.Row>
   )).reverse().slice(0, 15);
 
@@ -23,8 +23,8 @@ const DataTable: React.FC<DataTableProps> = ({ dateHeader, data, style }) => {
     <Table style={{ ...style, marginTop: 32 }}>
       <Table.Header>
         <Table.HeaderCell>{dateHeader}</Table.HeaderCell>
-        <Table.HeaderCell style={{ color: '#2E567E' }}>Pegel #1</Table.HeaderCell>
-        <Table.HeaderCell style={{ color: '#C05353' }}>Pegel #2</Table.HeaderCell>
+        <Table.HeaderCell style={{ color: '#2E567E' }}>Messstelle 1</Table.HeaderCell>
+        <Table.HeaderCell style={{ color: '#C05353' }}>Messstelle 2</Table.HeaderCell>
       </Table.Header>
       <Table.Body>
         {rows}
